609847330594ee7f66088197e1de38c7c45cae4c
Labonya <1aklimakter@gmail.com>
2023-10-20 03:04:11 +0600

    I have added dotenv to the index.js file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors =require("cors")
 const data= require("./data.json")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -9,13 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-//ikraamoni
-//MUUiu0YOmdO5FHfr
 
 
-const uri = "mongodb+srv://ikraamoni:MUUiu0YOmdO5FHfr@cluster0.waps95s.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.waps95s.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
 
      const fashionCollections = client.db("fashionBrandDB").collection("fashionCollections")
@@ -57,7 +57,6 @@ app.post("/product", async(req, res)=>{
 app.put("/product/:id", async(req, res)=>{
   const id =req.params.id 
   const products = req.body
-  console.log(id, products);
   const filter = {_id : new ObjectId(id)}
   const option ={upsert: true}
   const updateProduct ={
